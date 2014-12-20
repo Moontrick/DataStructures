ffc88a618709c4560f174134181093efec491246
moontrick <marcusanovak@gmail.com>
2014-12-20 11:35:44 -0600

    Add Linked List class file

---
+++ List/List/LList.cpp
@@ -0,0 +1,9 @@
+//
+//  LList.cpp
+//  List
+//
+//  Created by Marcus Novak on 12/20/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#include "LList.h"
---
+++ List/List/LList.h
@@ -0,0 +1,14 @@
+//
+//  LList.h
+//  List
+//
+//  Created by Marcus Novak on 12/20/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#ifndef __List__LList__
+#define __List__LList__
+
+#include <iostream>
+
+#endif /* defined(__List__LList__) */
