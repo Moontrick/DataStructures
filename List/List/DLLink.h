3a3987cc0cd2eeee31bb00b7fb3aa3c0115933e6
moontrick <marcusanovak@gmail.com>
2014-12-20 15:18:54 -0600

    Add comment to DLLink

--- List/List/DLLink.h
+++ List/List/DLLink.h
@@ -5,6 +5,8 @@
 //  Created by Marcus Novak on 12/20/14.
 //
 //  Doubly linked list link node implementation with freelist support
+//  Probably could have inherited from Link if I'd arranged it right
+//  but I'm not going to worry about it in this demonstration
 //
 //  Copyright (c) 2014 Marcus Novak. All rights reserved.
 //
