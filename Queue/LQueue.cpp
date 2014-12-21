c217ab928da7bc387bf4bde9fad0d8969efa01b8
moontrick <marcusanovak@gmail.com>
2014-12-21 12:41:20 -0600

    Add linked Queue class file

---
+++ Queue/LQueue.cpp
@@ -0,0 +1,9 @@
+//
+//  LQueue.cpp
+//  List
+//
+//  Created by Marcus Novak on 12/21/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#include "LQueue.h"
---
+++ Queue/LQueue.h
@@ -0,0 +1,14 @@
+//
+//  LQueue.h
+//  List
+//
+//  Created by Marcus Novak on 12/21/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#ifndef __List__LQueue__
+#define __List__LQueue__
+
+#include <iostream>
+
+#endif /* defined(__List__LQueue__) */
