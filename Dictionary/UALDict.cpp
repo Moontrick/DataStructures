f786d290dab2e5b843ceccb9a145efc2076cd0fb
moontrick <marcusanovak@gmail.com>
2014-12-21 16:53:55 -0600

    Implement unsorted array-based dictionary class

--- Dictionary/UALDict.cpp
+++ Dictionary/UALDict.cpp
@@ -7,3 +7,100 @@
 //
 
 #include "UALDict.h"
+
+
+
+// Constructor
+template <typename Key, typename T>
+UALDict<Key, T>::UALDict(int size)
+{ list = new AList<KVPair<Key, T>>(size); }
+
+
+
+// Destructor
+template <typename Key, typename T>
+UALDict<Key, T>::~UALDict()
+{ delete list; }
+
+
+// Insert an element, append to list
+template <typename Key, typename T>
+void UALDict<Key, T>::Insert(const Key& k, const T& t)
+{
+    KVPair<Key, T> tmp(k, t);
+    list->Append(tmp);
+}
+
+
+// Sequentially search to find the element to remove
+template <typename Key, typename T>
+T UALDict<Key, T>::Remove(const Key& k)
+{
+    T tmp = Find(k);
+    if (tmp != NULL) list->Remove();
+    return tmp;
+}
+
+
+// Remove the last element
+template <typename Key, typename T>
+T UALDict<Key, T>::RemoveAny()
+{
+    Assert(Size() != 0, "Dictionary is empty!");
+    
+    list->MoveToEnd();
+    list->prev();
+    KVPair<Key, T> t = list->Remove();
+    return t.Value();
+}
+
+
+// Find "k" using sequential search, return NULL if it doesn't exist
+template <typename Key, typename T>
+T UALDict<Key, T>::Find(const Key& k) const
+{
+    for(list->MoveToStart(); list->CurrPos() < list->Length(); list->Next())
+    {
+        KVPair<Key, T> tmp = list->GetValue();
+        if (k == tmp.Key())
+            return tmp.Value();
+    }
+    
+    return NULL;
+}
+
+
+// Return the number of elements in the list
+template <typename Key, typename T>
+int UALDict<Key, T>::Size()
+{ return list->Length(); }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Dictionary/UALDict.h
+++ Dictionary/UALDict.h
@@ -15,11 +15,25 @@
 #include <iostream>
 #include "Dictionary.h"
 #include "KVPair.h"
+#include "../List/AList.h"
 
 #define DEFAULT_SIZE 100
 
 template <typename Key, typename T> class UALDict : public Dictionary<Key, T>
 {
+private:
+    AList<KVPair<Key, T>>* list;       // Array to hold key-value pairs
+    
+public:
+    UALDict(int size = DEFAULT_SIZE);
+    ~UALDict();
+    
+    void Clear();
+    void Insert(const Key& k, const T& t);
+    T Remove(const Key& k);
+    T RemoveAny();
+    T Find(const Key& k) const;
+    int Size();
 };
 
 #endif /* defined(__List__UALDict__) */
