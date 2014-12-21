9a3cedeea499619c3cc44581ad3002dee1f53153
moontrick <marcusanovak@gmail.com>
2014-12-21 12:22:55 -0600

    Implement the array-based Queue

--- Queue/AQueue.cpp
+++ Queue/AQueue.cpp
@@ -7,3 +7,100 @@
 //
 
 #include "AQueue.h"
+
+
+// Constructor
+template <typename T>
+AQueue<T>::AQueue(int size)
+{
+    maxSize = size + 1;    // Array size one position larger for empty slot
+    rear = 0;
+    front = 1;
+    listArray = new T[maxSize];
+}
+
+
+// Destructor
+template <typename T>
+AQueue<T>::~AQueue()
+{ delete [] listArray; }
+
+
+// Reinitialize the queue
+template <typename T>
+void AQueue<T>::Clear()
+{
+    rear = 0;
+    front = 1;
+}
+
+
+// Place "it" at the rear of the Queue
+template <typename T>
+void AQueue<T>::Enqueue(const T& it)
+{
+    Assert(((rear + 2) % maxSize) != front, "Queue is full!");
+    rear = (rear + 1) % maxSize;        // Circular increment
+    listArray[rear] = it;
+}
+
+
+// Remove and return the element at the front of the queue
+template <typename T>
+T AQueue<T>::Dequeue()
+{
+    Assert(Length() != 0, "Queue is empty!");
+    T it = listArray[front];
+    front = (front + 1) % maxSize;      // Circular increment
+    return it;
+}
+
+
+// Return a copy of the front element in the queue
+template <typename T>
+const T& AQueue<T>::FrontValue() const
+{
+    Assert(Length() != 0, "Queue is empty!");
+    return listArray[front];
+}
+
+
+// Return the number of elements in the queue
+template <typename T>
+int AQueue<T>::Length() const
+{ return ((rear + maxSize) - front + 1) % maxSize; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Queue/AQueue.h
+++ Queue/AQueue.h
@@ -3,6 +3,9 @@
 //  List
 //
 //  Created by Marcus Novak on 12/21/14.
+//
+//  Implementation of an array-based Queue
+//
 //  Copyright (c) 2014 Marcus Novak. All rights reserved.
 //
 
@@ -10,5 +13,27 @@
 #define __List__AQueue__
 
 #include <iostream>
+#include "Queue.h"
+
+#define DEFAULT_SIZE 100
+
+template <typename T> class AQueue : public Queue<T>
+{
+private:
+    int maxSize;                // Max size of the Queue
+    int front;                  // Index of front element
+    int rear;                   // Index of rear element
+    T *listArray;               // Array holding queue of elements
+    
+public:
+    AQueue(int size = DEFAULT_SIZE);
+    ~AQueue();
+    
+    void Clear();
+    void Enqueue(const T& it);
+    T Dequeue();
+    const T& FrontValue() const;
+    int Length() const;
+};
 
 #endif /* defined(__List__AQueue__) */
