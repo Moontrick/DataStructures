706f03c3252d12815b8ae9662c81226ed08af5da
moontrick <marcusanovak@gmail.com>
2014-12-21 12:05:34 -0600

    Create files for array-based Queue

---
+++ Queue/AQueue.cpp
@@ -0,0 +1,9 @@
+//
+//  AQueue.cpp
+//  List
+//
+//  Created by Marcus Novak on 12/21/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#include "AQueue.h"
---
+++ Queue/AQueue.h
@@ -0,0 +1,14 @@
+//
+//  AQueue.h
+//  List
+//
+//  Created by Marcus Novak on 12/21/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#ifndef __List__AQueue__
+#define __List__AQueue__
+
+#include <iostream>
+
+#endif /* defined(__List__AQueue__) */
