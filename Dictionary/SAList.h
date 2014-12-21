bcfce278014d840b03a2e9cbc145abcbf64989e2
moontrick <marcusanovak@gmail.com>
2014-12-21 17:11:39 -0600

    Implement unsorted array-based list class

--- Dictionary/SAList.h
+++ Dictionary/SAList.h
@@ -17,10 +17,38 @@
 #include "KVPair.h"
 #include "../List/AList.h"
 
+
 template <typename Key, typename T> class SAList : protected AList<KVPair<Key, T>>
 {
 public:
-    SAList
+    SAList(int size = DEFAULT_SIZE) : AList<KVPair<Key, T>>(size) {}   // Constructor
+    ~SAList() {}                                                       // Destructor
+    
+    // Redefine the Insert function to keep the values sorted
+    void Insert(KVPair<Key, T>& it)
+    {
+        KVPair<Key, T> curr;
+        for (MoveToStart(); CurrPos() < Length(); Next())
+        {
+            curr = GetValue();
+            if (curr.Key() > it.Key())
+                break;
+        }
+        AList<KVPair<Key, T>>::Insert(it);   // Insert the element
+    }
+    
+    // All remaining methods are exposed from AList.  Append is not available to SAList
+    // as that would defeat the purpose of keeping the list nice and sorted
+    AList<KVPair<Key, T>>::Clear;
+    AList<KVPair<Key, T>>::Remove;
+    AList<KVPair<Key, T>>::MoveToStart;
+    AList<KVPair<Key, T>>::MoveToEnd;
+    AList<KVPair<Key, T>>::Prev;
+    AList<KVPair<Key, T>>::Next;
+    AList<KVPair<Key, T>>::Length;
+    AList<KVPair<Key, T>>::CurrPos;
+    AList<KVPair<Key, T>>::MoveTosPos;
+    AList<KVPair<Key, T>>::GetValue;
 };
 
 #endif /* defined(__List__SAList__) */
