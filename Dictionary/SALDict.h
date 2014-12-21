cb667860a30e7632d51ec3586e3cb6e59a900a38
moontrick <marcusanovak@gmail.com>
2014-12-21 17:24:43 -0600

    Add sorted array-based dictionary class

---
+++ Dictionary/SALDict.h
@@ -0,0 +1,14 @@
+//
+//  SALDict.h
+//  List
+//
+//  Created by Marcus Novak on 12/21/14.
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#ifndef __List__SALDict__
+#define __List__SALDict__
+
+#include <iostream>
+
+#endif /* defined(__List__SALDict__) */
