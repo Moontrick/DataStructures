be6e434b742ef1d2a206fa5f34e9acdb32e2c002
moontrick <marcusanovak@gmail.com>
2014-12-21 14:40:11 -0600

    Create abstract template Dictionary file

---
+++ Dictionary/Dictionary.h
@@ -0,0 +1,20 @@
+//
+//  Dictionary.h
+//  List
+//
+//  Created by Marcus Novak on 12/21/14.
+//
+//  Abstract template for Dictionary data structure
+//
+//  Copyright (c) 2014 Marcus Novak. All rights reserved.
+//
+
+#ifndef List_Dictionary_h
+#define List_Dictionary_h
+
+template <typename Key, typename T> class Dictionary
+{
+    
+};
+
+#endif
